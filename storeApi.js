b2d4376f4ebd1df39e18078d04be977d5bcb46e7
alnnasr <ia1@hotmail.de>
2019-01-04 17:39:03 +0100

    Add files via upload

---
+++ storeApi.js
@@ -0,0 +1,17 @@
+var store = {
+		setData: function(nameData,valueData){
+			this.nameData = nameData;
+			this.valueData = valueData;
+			this.convertValueData = JSON.stringify(this.valueData);
+			localStorage.setItem(this.nameData, this.convertValueData);
+		},
+		getData: function(getNameData){
+			this.getNameData = getNameData;
+			this.convertNameData = localStorage.getItem(this.getNameData);
+			return JSON.parse(this.convertNameData);
+		},
+		removeData: function(removeData){
+			this.removeData = removeData;
+			localStorage.removeItem(this.removeData);
+		}
+	}
